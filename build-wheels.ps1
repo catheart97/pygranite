982b410fb8ffefedfe55eb6cdf50f663f4364879
Ronja Schnur <ronjaschnur@uni-mainz.de>
2023-01-17 11:37:00 +0100

    :sparkles: Added citation file.

--- build-wheels.ps1
+++
@@ -1,9 +0,0 @@
-#!/bin/pwsh
-
-foreach ($env in Get-ChildItem venv -Directory -Name) {
-    if ($env.StartsWith("win")) {
-        Invoke-Expression ("./venv/" + $env + "/Scripts/activate.ps1")
-        python -m pip install build wheel
-        python -m build --wheel
-    }
-}
